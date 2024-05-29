4964320efb744c27162f9ee0d1fb52e075c31f9e
Dmitriy Nikolaev <dimik10@mail.ru>
2024-05-29 12:56:37 +0300

    роль изменение экспотера

---
+++ roles/ceph-node-exporter/files/smartmon.sh
@@ -0,0 +1,236 @@
+#!/usr/bin/env bash
+#
+# Script informed by the collectd monitoring script for smartmontools (using smartctl)
+# by Samuel B. <samuel_._behan_(at)_dob_._sk> (c) 2012
+# source at: http://devel.dob.sk/collectd-scripts/
+
+# TODO: This probably needs to be a little more complex.  The raw numbers can have more
+#       data in them than you'd think.
+#       http://arstechnica.com/civis/viewtopic.php?p=22062211
+
+# Formatting done via shfmt -i 2
+# https://github.com/mvdan/sh
+
+# Ensure predictable numeric / date formats, etc.
+export LC_ALL=C
+
+parse_smartctl_attributes_awk="$(
+  cat <<'SMARTCTLAWK'
+$1 ~ /^ *[0-9]+$/ && $2 ~ /^[a-zA-Z0-9_-]+$/ {
+  gsub(/-/, "_");
+  printf "%s_value{%s,smart_id=\"%s\"} %d\n", $2, labels, $1, $4
+  printf "%s_worst{%s,smart_id=\"%s\"} %d\n", $2, labels, $1, $5
+  printf "%s_threshold{%s,smart_id=\"%s\"} %d\n", $2, labels, $1, $6
+  printf "%s_raw_value{%s,smart_id=\"%s\"} %e\n", $2, labels, $1, $10
+}
+SMARTCTLAWK
+)"
+
+smartmon_attrs="$(
+  cat <<'SMARTMONATTRS'
+airflow_temperature_cel
+command_timeout
+current_pending_sector
+end_to_end_error
+erase_fail_count
+g_sense_error_rate
+hardware_ecc_recovered
+host_reads_32mib
+host_reads_mib
+host_writes_32mib
+host_writes_mib
+load_cycle_count
+media_wearout_indicator
+nand_writes_1gib
+offline_uncorrectable
+power_cycle_count
+power_on_hours
+program_fail_cnt_total
+program_fail_count
+raw_read_error_rate
+reallocated_event_count
+reallocated_sector_ct
+reported_uncorrect
+runtime_bad_block
+sata_downshift_count
+seek_error_rate
+spin_retry_count
+spin_up_time
+start_stop_count
+temperature_case
+temperature_celsius
+temperature_internal
+total_lbas_read
+total_lbas_written
+udma_crc_error_count
+unsafe_shutdown_count
+unused_rsvd_blk_cnt_tot
+wear_leveling_count
+workld_host_reads_perc
+workld_media_wear_indic
+workload_minutes
+SMARTMONATTRS
+)"
+smartmon_attrs="$(echo "${smartmon_attrs}" | xargs | tr ' ' '|')"
+
+parse_smartctl_attributes() {
+  local disk="$1"
+  local disk_type="$2"
+  local labels="disk=\"${disk}\",type=\"${disk_type}\""
+  sed 's/^ \+//g' |
+    awk -v labels="${labels}" "${parse_smartctl_attributes_awk}" 2>/dev/null |
+    tr '[:upper:]' '[:lower:]' |
+    grep -E "(${smartmon_attrs})"
+}
+
+parse_smartctl_scsi_attributes() {
+  local disk="$1"
+  local disk_type="$2"
+  local labels="disk=\"${disk}\",type=\"${disk_type}\""
+  while read -r line; do
+#Входная строка выглядит так:
+#Current Drive Temperature:     25 C
+#Drive Trip Temperature:        85 C
+#Manufactured in week 23 of year 2020
+#Specified cycle count over device lifetime:  50000
+#Accumulated start-stop cycles:  6
+#Specified load-unload count over device lifetime:  600000
+#Accumulated load-unload cycles:  733
+#Elements in grown defect list: 0
+ #тут собирается тип  это первое значение пример: Current Drive Temperature:
+    attr_type="$(echo "${line}" | tr '=' ':' | cut -f1 -d: | sed 's/^ \+//g' | tr ' ' '_' | grep -v '^$')"
+    # тут второе значение пример: 25 C
+    attr_value="$(echo "${line}" | tr '=' ':' | cut -f2 -d: | sed 's/^ \+//g' | grep -v '^$')"
+    case "${attr_type}" in
+    #тут зачем то перевод в 16-ю систему
+    Current_Drive_Temperature) temp_cel="$(echo "${attr_value}" | cut -f1 -d' ' | awk '{ print $1 }')" ;;
+    "read" ) read_error="$(echo "${attr_value}" | awk '{print $7}')" ;;
+    "read" ) gb_processed_read="$(echo "${attr_value}" | awk '{print $6}')" ;;
+    "write" ) write_error="$(echo "${attr_value}" | awk '{print $7}')" ;;
+    "write" ) gb_processed_write="$(echo "${attr_value}" | awk '{print $6}')" ;;
+    Accumulated_load-unload_cycles) load_cycle="$(echo "${attr_value}" | awk '{ print $1 }')" ;;
+    Accumulated_start-stop_cycles) power_cycle="$(echo "${attr_value}" | awk '{ print $1 }')" ;;
+    esac
+    case "${attr_type}" in
+    "read" ) gb_processed_read="$(echo "${attr_value}" | awk '{print $6}')" ;;
+    "write" ) gb_processed_write="$(echo "${attr_value}" | awk '{print $6}')" ;;
+    esac
+
+  done
+  [ -n "$temp_cel" ] && echo "temperature_celsius_value{${labels},smart_id=\"12\"} ${temp_cel}"
+  [ -n "$read_error" ] && echo "total_uncorrected_errors_read_value{${labels},smart_id=\"14\"} ${read_error}"
+  [ -n "$gb_processed_read" ] && echo "gigabytes_processed_read_value{${labels},smart_id=\"15\"} ${gb_processed_read}"
+  [ -n "$gb_processed_write" ] && echo "gigabytes_processed_write_value{${labels},smart_id=\"16\"} ${gb_processed_write}"
+  [ -n "$write_error" ] && echo "total_uncorrected_errors_write_value{${labels},smart_id=\"17\"} ${write_error}"
+  [ -n "$power_cycle" ] && echo "power_cycle_count_value{${labels},smart_id=\"18\"} ${power_cycle}"
+  [ -n "$load_cycle" ] && echo "load_unload_count_value{${labels},smart_id=\"13\"} ${load_cycle}"
+}
+
+
+parse_smartctl_info() {
+  local -i smart_available=0 smart_enabled=0 smart_healthy=
+  local disk="$1" disk_type="$2"
+  local model_family='' device_model='' serial_number='' fw_version='' vendor='' product='' revision='' lun_id=''
+  while read -r line; do
+    info_type="$(echo "${line}" | cut -f1 -d: | tr ' ' '_')"
+    info_value="$(echo "${line}" | cut -f2- -d: | sed 's/^ \+//g' | sed 's/"/\\"/')"
+    case "${info_type}" in
+    Model_Family) model_family="${info_value}" ;;
+    Device_Model) device_model="${info_value}" ;;
+    Serial_Number|Serial_number) serial_number="${info_value}" ;;
+    Firmware_Version) fw_version="${info_value}" ;;
+    Vendor) vendor="${info_value}" ;;
+    Product) product="${info_value}" ;;
+    Revision) revision="${info_value}" ;;
+    Logical_Unit_id) lun_id="${info_value}" ;;
+    esac
+    if [[ "${info_type}" == 'SMART_support_is' ]]; then
+      case "${info_value:0:7}" in
+      Enabled) smart_available=1; smart_enabled=1 ;;
+      Availab) smart_available=1; smart_enabled=0 ;;
+      Unavail) smart_available=0; smart_enabled=0 ;;
+      esac
+    fi
+    if [[ "${info_type}" == 'SMART_overall-health_self-assessment_test_result' ]]; then
+      case "${info_value:0:6}" in
+      PASSED) smart_healthy=1 ;;
+      *) smart_healthy=0 ;;
+      esac
+    elif [[ "${info_type}" == 'SMART_Health_Status' ]]; then
+      case "${info_value:0:2}" in
+      OK) smart_healthy=1 ;;
+      *) smart_healthy=0 ;;
+      esac
+    fi
+  done
+  echo "device_info{disk=\"${disk}\",type=\"${disk_type}\",vendor=\"${vendor}\",product=\"${product}\",revision=\"${revision}\",lun_id=\"${lun_id}\",model_family=\"${model_family}\",device_model=\"${device_model}\",serial_number=\"${serial_number}\",firmware_version=\"${fw_version}\"} 1"
+  echo "device_smart_available{disk=\"${disk}\",type=\"${disk_type}\"} ${smart_available}"
+  echo "device_smart_enabled{disk=\"${disk}\",type=\"${disk_type}\"} ${smart_enabled}"
+  [[ "${smart_healthy}" != "" ]] && echo "device_smart_healthy{disk=\"${disk}\",type=\"${disk_type}\"} ${smart_healthy}"
+}
+
+output_format_awk="$(
+  cat <<'OUTPUTAWK'
+BEGIN { v = "" }
+v != $1 {
+  print "# HELP smartmon_" $1 " SMART metric " $1;
+  print "# TYPE smartmon_" $1 " gauge";
+  v = $1
+}
+{print "smartmon_" $0}
+OUTPUTAWK
+)"
+
+format_output() {
+  sort |
+    awk -F'{' "${output_format_awk}"
+}
+
+smartctl_version="$(/usr/sbin/smartctl -V | head -n1 | awk '$1 == "smartctl" {print $2}')"
+
+echo "smartctl_version{version=\"${smartctl_version}\"} 1" | format_output
+
+if [[ "$(expr "${smartctl_version}" : '\([0-9]*\)\..*')" -lt 6 ]]; then
+  exit
+fi
+
+
+#device_list="$(/usr/sbin/smartctl --scan-open | awk '/^\/dev/{print $1 "|" $3}')"
+#device_list="/dev/sdcs|scsi"
+
+#dev="/dev/"
+device_list="$(/usr/sbin/multipath -l | grep -A3 "mpath" | grep "sd" | awk '{print "/dev/" $3}')"
+#scsi="|scsi"
+#device_list="${dev} ${device_list1} ${scsi}"
+
+
+for device in ${device_list}; do
+#Разбор строк из device_list (/dev/sdcx|scsi) и разделение на диск
+  #disk="$(echo "${device}" | cut -f1 -d'|')"
+  disk="$(echo "${device}")"
+ # и тип
+  #type="$(echo "${device}" | cut -f2 -d'|')"
+  type=scsi
+  active=1
+  echo "smartctl_run{disk=\"${disk}\",type=\"${type}\"}" "$(TZ=UTC date '+%s')"
+  # Check if the device is in a low-power mode
+  /usr/sbin/smartctl -n standby -d "${type}" "${disk}" > /dev/null || active=0
+  echo "device_active{disk=\"${disk}\",type=\"${type}\"}" "${active}"
+  # Skip further metrics to prevent the disk from spinning up
+  test ${active} -eq 0 && continue
+  # Получение SMART информации и статуса по parse_smartctl_info
+  /usr/sbin/smartctl -i -H -d "${type}" "${disk}" | parse_smartctl_info "${disk}" "${type}"
+  # Get the SMART attributes по parse_smartctl_attributes или parse_smartctl_scsi_attributes
+  case ${type} in
+  sat) /usr/sbin/smartctl -A -d "${type}" "${disk}" | parse_smartctl_attributes "${disk}" "${type}" ;;
+  sat+megaraid*) /usr/sbin/smartctl -A -d "${type}" "${disk}" | parse_smartctl_attributes "${disk}" "${type}" ;;
+  scsi) /usr/sbin/smartctl --all -d "${type}" "${disk}" | parse_smartctl_scsi_attributes "${disk}" "${type}" ;;
+  megaraid*) /usr/sbin/smartctl -A -d "${type}" "${disk}" | parse_smartctl_scsi_attributes "${disk}" "${type}" ;;
+  nvme*) /usr/sbin/smartctl -A -d "${type}" "${disk}" | parse_smartctl_scsi_attributes "${disk}" "${type}" ;;
+  usbprolific) /usr/sbin/smartctl -A -d "${type}" "${disk}" | parse_smartctl_attributes "${disk}" "${type}" ;;
+  *)
+      (>&2 echo "disk type is not sat, scsi, nvme or megaraid but ${type}")
+    exit
+    ;;
+  esac
+done | format_output
